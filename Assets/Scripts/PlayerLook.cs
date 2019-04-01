e198834eca58376e1c11d0060a75ead5a933d562
Sergio Marchena <mar16387@uvg.edu.gt>
2019-03-31 22:44:28 -0600

    build final
    build final

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour 
+{
+	public string mouseXInputName, mouseYInputName;
+    public float mouseSensitivity;
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+	private void Awake()
+	{
+		LockCursor();
+		xAxisClamp = 0.0f;
+	}
+
+	private void LockCursor() 
+	{
+		Cursor.lockState = CursorLockMode.Locked; 
+	}
+	private void Update() {
+		CameraRotation();
+		if (Input.GetKey("escape"))
+            Application.Quit();
+	}
+
+	private void CameraRotation() 
+	{
+		float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+	 	float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+	 	xAxisClamp += mouseY;
+
+	 	if (xAxisClamp > 90.0f) {
+	 		xAxisClamp = 90.0f;
+	 		mouseY = 0.0f;
+	 		ClampX(180.0f);
+	 	}
+	 	else if (xAxisClamp < -90.0f) {
+	 		xAxisClamp = -90.0f;
+	 		mouseY = 0.0f;
+	 		ClampX(90.0f);
+	 	}
+
+	 	transform.Rotate(Vector3.left * mouseY);
+	 	playerBody.Rotate(Vector3.up *mouseX);
+	 }
+
+	 private void ClampX (float value)
+	 {
+	 	Vector3 eulerRotation = transform.eulerAngles;
+	 	eulerRotation.x = value;
+	 	transform.eulerAngles = eulerRotation;
+	 }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMove : MonoBehaviour
+{
+	public string horizontalInput, verticalInput;
+	public float speed;
+
+	private CharacterController charController;
+
+	[SerializeField] private AnimationCurve jumpFallOff;
+	[SerializeField] private float jumpMulti;
+	[SerializeField] private KeyCode jumpKey;
+
+	private bool isJumping;
+
+	private void Awake()
+	{
+		charController = GetComponent<CharacterController>();
+	}
+
+	private void Update()
+	{
+		PlayerMovement();
+	}
+
+	private void PlayerMovement() 
+	{
+		float verInput = Input.GetAxis(verticalInput) * speed;
+		float horInput = Input.GetAxis(horizontalInput) * speed;
+
+		Vector3 forwardMovement = transform.forward * verInput;
+		Vector3 rightMovement = transform.right * horInput;
+
+		charController.SimpleMove(forwardMovement + rightMovement);
+
+		jumpInput();
+	}
+
+	private void jumpInput()
+	{
+		if (Input.GetKeyDown(jumpKey) && !isJumping)
+		{
+			isJumping = true;
+			StartCoroutine(JumpEvent());
+		}
+
+	}
+    
+    private IEnumerator JumpEvent()
+    {
+    	float timeInAir = 0.0f;
+    	do 
+    	{
+    		float jumpForce = jumpFallOff.Evaluate(timeInAir);
+    		charController.Move(Vector3.up * jumpForce * jumpMulti * Time.deltaTime);
+    		timeInAir+= Time.deltaTime;
+    		yield return null;
+    	} while(!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+    	isJumping = false;
+    }
+
+}
